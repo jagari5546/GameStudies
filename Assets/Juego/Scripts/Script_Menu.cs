6001990b31ede671df584293d5708bdde590f5c9
jagari5546 <jagari5546@gmail.com>
2023-11-13 18:59:11 -0500

    Cambios2

--- Assets/Juego/Scripts/Script_Menu.cs
+++ Assets/Juego/Scripts/Script_Menu.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
-{
+{ 
+  public void Iniciar(string NombreNivel)
+    {
+        SceneManager.LoadScene(NombreNivel);
+    }
   public void Salir()
     {
         Application.Quit();
+        Debug.Log("Aca se cierra el juego");
     }
 }
